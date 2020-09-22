fcf6b88cb2419d833454efa85218748585149f82
Christopher Fernández <fernandez.chl@gmail.com>
2020-09-22 19:40:21 -0300

    feat: add TMC controllers and TMC service using Sbif API

--- app/controllers/api/v1/tmcs_controller.rb
+++ app/controllers/api/v1/tmcs_controller.rb
@@ -3,7 +3,26 @@
 module Api
   module V1
     class TmcsController < ApplicationController
-      def tmc; end
+      def tmc
+        tmc_adapter = SbifAPI::TMCAdapter.new(tmc_params)
+        tmc_adapter.validate!
+
+        tmc_service = SbifAPI::TMC.new(tmc_adapter)
+        tmc_value = tmc_service.tmc_value(tmc_adapter.date.to_date)
+
+        render json: {
+          loan_amount: tmc_service.loan_amount,
+          loan_term: tmc_service.loan_term,
+          date: tmc_adapter.date,
+          tmc: tmc_value
+        }
+      end
+
+      private
+
+      def tmc_params
+        params.permit(:loan_term, :loan_amount, :date)
+      end
     end
   end
 end
