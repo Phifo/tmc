9a478d914d6f8e454594af1558be46c170caa73c
Christopher Fernández <fernandez.chl@gmail.com>
2020-09-22 17:45:07 -0300

    test: update tests

--- spec/requests/api/v1/tmcs_request_spec.rb
+++ spec/requests/api/v1/tmcs_request_spec.rb
@@ -4,29 +4,36 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Tmcs', type: :request do
   describe 'GET /api/v1/tmcs' do
+    shared_examples 'tmc' do
+      it 'retrieves correct tmc value' do
+        get api_v1_tmc_url, params: { loan_term: loan_term, loan_amount: loan_amount, date: date }
+
+        expect(response.body).to include_json(expected_response)
+      end
+    end
+
+    before do
+      response = double('response', status: 200, body: file_fixture('sbif_response.json').read)
+      allow_any_instance_of(Faraday::Connection).to receive(:get).and_return(response)
+    end
+
+    let(:date) { '2020-08-14' }
+
     context 'with loan term less than or equal to 3 months' do
       let(:loan_term) { 2 }
 
       context 'with loan amount less than or equal to 5000 UF' do
         let(:loan_amount) { 5000 }
+        let(:expected_response) { { tmc: 35.04 } }
 
-        it 'retrieves the tmc percentage' do
-          expected_response = { tmc: 35.04 }
-
-          get api_v1_tmc_url
-          expect(response.body).to include_json(expected_response)
-        end
+        include_examples 'tmc'
       end
 
       context 'with loan amount greater than 5000 UF' do
         let(:loan_amount) { 5001 }
+        let(:expected_response) { { tmc: 6.81 } }
 
-        it 'retrieves the tmc percentage' do
-          expected_response = { tmc: 6.81 }
-
-          get api_v1_tmc_url
-          expect(response.body).to include_json(expected_response)
-        end
+        include_examples 'tmc'
       end
     end
 
@@ -35,46 +42,30 @@ RSpec.describe 'Api::V1::Tmcs', type: :request do
 
       context 'with loan amount less than or equal to 50 UF' do
         let(:loan_amount) { 50 }
+        let(:expected_response) { { tmc: 33.8 } }
 
-        it 'retrieves the tmc percentage' do
-          expected_response = { tmc: 33.8 }
-
-          get api_v1_tmc_url
-          expect(response.body).to include_json(expected_response)
-        end
+        include_examples 'tmc'
       end
 
       context 'with loan amount greater than 50 UF and less than or equal to 200 UF' do
         let(:loan_amount) { 51 }
+        let(:expected_response) { { tmc: 26.8 } }
 
-        it 'retrieves the tmc percentage' do
-          expected_response = { tmc: 26.8 }
-
-          get api_v1_tmc_url
-          expect(response.body).to include_json(expected_response)
-        end
+        include_examples 'tmc'
       end
 
       context 'with loan amount greater than 200 UF and less than or equal to 5000 UF' do
         let(:loan_amount) { 201 }
+        let(:expected_response) { { tmc: 19.2 } }
 
-        it 'retrieves the tmc percentage' do
-          expected_response = { tmc: 19.2 }
-
-          get api_v1_tmc_url
-          expect(response.body).to include_json(expected_response)
-        end
+        include_examples 'tmc'
       end
 
       context 'with loan amount greater than 5000 UF' do
         let(:loan_amount) { 5001 }
+        let(:expected_response) { { tmc: 4.97 } }
 
-        it 'retrieves the tmc percentage' do
-          expected_response = { tmc: 4.97 }
-
-          get api_v1_tmc_url
-          expect(response.body).to include_json(expected_response)
-        end
+        include_examples 'tmc'
       end
     end
   end
