cc52265f8b9197a57253d17a1454fdb4c675b897
Christopher Fernández <fernandez.chl@gmail.com>
2020-09-23 02:38:08 -0300

    feat: add error management to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  rescue_from StandardError do |exception|
+    render json: {
+      errors: [{
+        status: 500,
+        detail: exception.message
+      }]
+    }, status: :internal_server_error
+  end
+
+  rescue_from ActiveModel::ValidationError do |exception|
+    render json: {
+      errors: [{
+        status: 400,
+        detail: exception.message
+      }]
+    }, status: :bad_request
+  end
+
+  rescue_from ActionController::RoutingError do |exception|
+    render json: {
+      errors: [{
+        status: 404,
+        detail: exception.message
+      }]
+    }, status: :not_found
+  end
 end
